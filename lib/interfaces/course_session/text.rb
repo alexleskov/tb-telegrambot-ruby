89ae634c34131a4d9983a0e565869e3e0aaf331a
alexleskov <39882467+alexleskov@users.noreply.github.com>
2020-08-11 00:21:23 +0300

    Update text.rb

--- lib/interfaces/course_session/text.rb
+++ lib/interfaces/course_session/text.rb
@@ -5,9 +5,6 @@ module Teachbase
     class Interfaces
       class CourseSession
         class Text < Teachbase::Bot::InterfaceController
-          def state(_state)
-            answer.text.send_out(entity.sign_course_state.to_s)
-          end
         end
       end
     end
