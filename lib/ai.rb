d53c3a9b0a0910bfa39a6bab338d2b1555fe6bec
Alexander Leskov <dsl_92@mail.ru>
2020-09-14 14:45:19 +0300

    Added small talks slugs list

--- lib/ai.rb
+++ lib/ai.rb
@@ -3,6 +3,8 @@
 module Teachbase
   module Bot
     class AI
+      SMALL_TALK_INTENTS_SLUG = %w[greetings creator]
+
       attr_reader :client
 
       def initialize
@@ -11,7 +13,7 @@ module Teachbase
 
       def find_reaction(text)
         nlp_result = analyse(text)
-        nlp_result.intents.any? { |intent| intent.slug == "greetings" } ? message_by_small_talk(text) : nlp_result
+        nlp_result.intents.any? { |intent| SMALL_TALK_INTENTS_SLUG.include?(intent.slug) } ? message_by_small_talk(text) : nlp_result
       end
 
       def analyse(text)
