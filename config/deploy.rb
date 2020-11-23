ff9efb47ccded4901b865fe0f032319a485f13bb
Alexander Leskov <dsl_92@mail.ru>
2020-11-24 01:41:41 +0300

    Puma deploy configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,19 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
+server '188.246.233.53', port: 5000
+
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
 set :application, "tb-telegrambot"
 set :repo_url, "https://github.com/alexleskov/tb-telegrambot-ruby.git"
 
@@ -15,6 +28,7 @@ append :linked_dirs, "log", "tmp"
 
 # Default value for default_env is {}
 set :default_env, RAILS_ENV: fetch(:stage)
+
 
 namespace :db do
   task :create do
@@ -54,5 +68,23 @@ namespace :deploy do
   end
 end
 
-before "deploy:restart_bot", "db:migrate"
-after "deploy:finishing", "deploy:restart_bot"
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+before "deploy:restart_bot", "puma:start", "db:migrate"
+after "deploy:finishing", "deploy:restart_bot", "puma:restart"
