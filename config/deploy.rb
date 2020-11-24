2c9981b145849b382360cac010939934e73ab8eb
Alexander Leskov <dsl_92@mail.ru>
2020-11-25 00:10:58 +0300

    Added puma configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,19 +3,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-server '188.246.233.53', port: 5000
-
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
-
 set :application, "tb-telegrambot"
 set :repo_url, "https://github.com/alexleskov/tb-telegrambot-ruby.git"
 
@@ -68,22 +55,12 @@ namespace :deploy do
 end
 
 namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
+    on roles(:all), in: :sequence, wait: 5 do
+      execute :sudo, :monit, 'restart tb_bot_web'
     end
   end
-
-  before :start, :make_dirs
 end
 
-before "deploy:restart_bot", "puma:start", "db:migrate"
+before "deploy:restart_bot", "db:migrate"
 after "deploy:finishing", "deploy:restart_bot", "puma:restart"
