cc47a48365abb537b722a5ab84b725f8e28f54fc
Alexander Leskov <dsl_92@mail.ru>
2020-11-25 23:46:48 +0300

    Update api server 11

--- lib/api_server.rb
+++ lib/api_server.rb
@@ -25,7 +25,7 @@ module Teachbase
         end
 
         def account_id
-          location = @env["REQUEST_PATH"].match(%r{^\/#{$app_config.default_location_webhooks_endpoint}\/(\w*)\/(\d*)})
+          location = @env["REQUEST_PATH"].match(%r{^#{$app_config.default_location_webhooks_endpoint}\/(\w*)\/(\d*)})
           return unless location
 
           location[2].to_i
@@ -54,13 +54,13 @@ module Teachbase
       end
 
       def init_request_by_webhook
-        on "/webhooks_catcher" do
+        on "webhooks_catcher" do
           Teachbase::Bot::ApiServer::Request.new(@env)
         end
       end
 
       def on(path)
-        location = @env["REQUEST_PATH"].match(%r{^\/#{$app_config.default_location_webhooks_endpoint}\/(#{path})\/(\d*)})
+        location = @env["REQUEST_PATH"].match(%r{^#{$app_config.default_location_webhooks_endpoint}\/(#{path})\/(\d*)})
         return unless location
 
         location[1]
