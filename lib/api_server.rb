77ac77a8647c10fd78d0fa5e0ee9fb0d685d5ec6
Alexander Leskov <dsl_92@mail.ru>
2020-11-26 00:11:28 +0300

    Some fixes for finding and api server

--- lib/api_server.rb
+++ lib/api_server.rb
@@ -42,7 +42,7 @@ module Teachbase
         return render(403, "403. Forbidden") unless request
 
         Teachbase::Bot::Webhook::Controller.new(request)
-        render(200, "OK\nRequset: #{request.data}")
+        render(200, "OK\nRequest: #{request.data}")
       rescue StandardError => e
         render(500, e.message)
       end
