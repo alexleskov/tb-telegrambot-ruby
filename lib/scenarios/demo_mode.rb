ea893ff28afd6dea5535970e306d9335bddca3dc
Alexander Leskov <dsl_92@mail.ru>
2020-12-06 23:09:20 +0300

    Some text fixes

--- lib/scenarios/demo_mode.rb
+++ lib/scenarios/demo_mode.rb
@@ -28,14 +28,14 @@ module Teachbase
 
           appshell.data_loader.user.profile.me
           interface.sys.menu.greetings(appshell.user_fullname(:array).first).show
-          interface.sys.menu(disable_web_page_preview: false, text: "Меня зовут <a href='https://storage.yandexcloud.net/tbpublic/other/teachbaser.jpg'>Teachbaser</a> - я Telegram-бот. Персональный помощник в прохождении обучения на платформе Teachbase.\n\nДаю возможность изучать назначенные курсы прямо в Telegram, просматривать библиотеку доступных документов, а когда будет назначен новый курс, то пришлю уведомление об этом, кроме этого, имею еще много разных полезных функций.\n\nНачнем знакомство по ближе? Выбирай ниже интересующий пункт и приступим.").after_auth.show
+          interface.sys.menu(disable_web_page_preview: false, text: I18n.t('about_bot_demo_mode')).after_auth.show
         rescue RuntimeError, TeachbaseBotException => e
           $logger.debug "On auth error: #{e.class}. #{e.inspect}"
           title = to_text_by_exceiption_code(e)
           title = "#{I18n.t('accounts')}: #{title}" if e.is_a?(TeachbaseBotException::Account)
           if access_denied?(e) || e.is_a?(TeachbaseBotException::Account)
             appshell.logout
-            title = "#{title} #{I18n.t('enter_by_auth_data').downcase}" if e.http_code == 401
+            title = "#{title} #{I18n.t('enter_by_auth_data').downcase} #{I18n.t('info_about_setted_password')}" if e.http_code == 401
             interface.sys.menu(text: title).sign_in_again.show
           end
           interface.sys.menu.starting.show
