e4fd5aa2421f6165a42e08662d826736da25d8ef
Alexander Leskov <dsl_92@mail.ru>
2020-12-06 22:43:40 +0300

    After Rubocop

--- db/migrate/029_add_tgids_to_accounts.rb
+++ db/migrate/029_add_tgids_to_accounts.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
 
 class AddTgidsToAccounts < ActiveRecord::Migration[5.2]
   def change
     add_column :accounts, :curator_tg_id, :integer
     add_column :accounts, :support_tg_id, :integer
-    change_column_default :accounts, :support_tg_id, from: nil, to: 439802952
+    change_column_default :accounts, :support_tg_id, from: nil, to: 439_802_952
   end
 end
