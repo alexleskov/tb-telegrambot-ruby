8f6d365c425a90a43e66517c218727a14f474223
Alexander Leskov <dsl_92@mail.ru>
2021-01-08 01:01:08 +0300

    Added info about cached messages in debug mode

--- lib/api_server.rb
+++ lib/api_server.rb
@@ -42,7 +42,7 @@ module Teachbase
         end
       end
 
-      @debug_info = ["Initial answer"]
+      @debug_info = []
       @debug_mode = false
 
       def call(env)
@@ -85,7 +85,7 @@ module Teachbase
 
       def render(status, message = "")
         message = "Code: #{status}. #{message}"
-        message = "#{message}<br>Debug mode is on.<br><br>#{self.class.debug_info.join('<br><br>')}" if self.class.debug_mode
+        message = "#{message}<br>Debug mode is on. Cache Messages: #{Teachbase::Bot::Cache.all.join('<br><br>')}<br><br>#{self.class.debug_info.join('<br><br>')}" if self.class.debug_mode
         [status, { 'Content-Type' => 'text/html; charset=UTF-8' }, [message]]
       end
 
