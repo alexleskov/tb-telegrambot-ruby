7001a744d75bf2710dc9647c28b6c682396147d2
Alexander Leskov <dsl_92@mail.ru>
2021-01-15 21:05:47 +0300

    Style fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,8 +7,8 @@ scheduler.every '3m', name: "New courses notification" do |job|
   raised_messages_by_tg_users_id = result[:raised].group_by { |raised_message| raised_message.tg_account.id }
   notifications_params = Teachbase::Bot::Helper::Notification.new(raised_messages_by_tg_users_id, :cs).build
   job.kill unless notifications_params
-  result[:raw].destroy_all
 
+  result[:raw].destroy_all
   notifications_params.each do |notify_param|
     I18n.with_locale notify_param[:settings].localization.to_sym do
       Teachbase::Bot::Strategies::Notify.new(notify_param[:controller], type: :cs).about(notify_param[:tb_ids])
