c80761e7efb9c48ecc305747ac6d5e8a1c5cde84
Alexander Leskov <dsl_92@mail.ru>
2021-02-19 21:43:36 +0300

    Updated auto notification delete after sending

--- lib/helpers/notification.rb
+++ lib/helpers/notification.rb
@@ -4,29 +4,29 @@ module Teachbase
   module Bot
     module Helper
       class Notification
-        attr_reader :type, :messages_by_tg_users_id
+        attr_reader :type, :messages_by_tg_accounts_id
 
-        def initialize(messages_by_tg_users_id, type)
+        def initialize(messages_by_tg_accounts_id, type)
           @type = type
-          @messages_by_tg_users_id = messages_by_tg_users_id
+          @messages_by_tg_accounts_id = messages_by_tg_accounts_id
         end
 
         def build
-          return unless messages_by_tg_users_id && !messages_by_tg_users_id.empty?
+          return unless messages_by_tg_accounts_id && !messages_by_tg_accounts_id.empty?
 
           params = []
-          messages_by_tg_users_id.each do |tg_user_id, messages|
+          messages_by_tg_accounts_id.each do |tg_account_id, messages|
             tb_ids = []
             messages.each do |message|
               tb_ids << message.webhook.payload["data"][id_param].to_i
             end
-            first_message = messages_by_tg_users_id[tg_user_id].first
+            first_message = messages_by_tg_accounts_id[tg_account_id].first
             catcher = Teachbase::Bot::Webhook::Catcher.new(first_message.webhook)
             context = catcher.init_webhook
             next unless context.tg_user
 
             strategy = context.handle
-            params << { controller: strategy.controller, settings: context.settings, tb_ids: tb_ids.uniq }
+            params << { controller: strategy.controller, settings: context.settings, tg_account_id: tg_account_id  tb_ids: tb_ids.uniq }
           end
           params
         end
