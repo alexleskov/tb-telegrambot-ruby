25b6a32dc4297509bf0c34cc317518c08a6338aa
Alexander Leskov <dsl_92@mail.ru>
2021-07-18 15:09:38 +0300

    Some fixes

--- lib/interfaces/course_session/text.rb
+++ lib/interfaces/course_session/text.rb
@@ -6,7 +6,7 @@ module Teachbase
       class CourseSession
         class Text < Teachbase::Bot::Interfaces::Text
           def course
-            @params[:text] = [text, entity.sign_open(route: router.g(:cs, :root, id: entity.tb_id).link).to_s].join("\n")
+            @params[:text] = [params[:text], entity.sign_open(route: router.g(:cs, :root, id: entity.tb_id).link).to_s].join("\n")
             self
           end
 
--- lib/interfaces/section/menu.rb
+++ lib/interfaces/section/menu.rb
@@ -5,31 +5,6 @@ module Teachbase
     class Interfaces
       class Section
         class Menu < Teachbase::Bot::Interfaces::Menu
-          # def main
-          #  raise "Entity must be a CourseSession" unless entity.is_a?(Teachbase::Bot::CourseSession)
-          #
-          #  @params[:type] = :menu_inline
-          #  @params[:slices_count] = 3
-          #  @params[:disable_web_page_preview] = false
-          #  @params[:buttons] = main_buttons
-          #  @params[:text] = [create_title(title_params), entity.statistics, entity.categories_name, description,
-          #                    entity.sign_aval_sections_count_from].compact.join("\n")
-          #
-          #  @params[:mode] ||= :none
-          #  self
-          # end
-
-          # def show_by_option(sections, option)
-          #  raise "Entity must be a CourseSession" unless entity.is_a?(Teachbase::Bot::CourseSession)
-          #
-          #  @params[:type] = :menu_inline
-          #  @disable_notification = true
-          #  @params[:mode] ||= option == :find_by_query_num ? :none : :edit_msg
-          #  @params[:text] ||= [create_title(title_params), build_list_with_state(sections.sort_by(&:position))].join("\n")
-          #  @params[:buttons] = InlineCallbackKeyboard.collect(buttons: [], back_button: back_button).raw
-          #  self
-          # end
-
           def contents
             raise "Entity must be a Section" unless entity.is_a?(Teachbase::Bot::Section)
 
@@ -42,28 +17,6 @@ module Teachbase
           end
 
           private
-
-          # def build_list_with_state(sections)
-          #  result = []
-          #  sections.each do |section|
-          #    result << section.title_with_state(state: section.find_state,
-          #                                       route: router.g(:section, :root, position: section.position,
-          #                                                                        p: [cs_id: entity.tb_id]).link)
-          #  end
-          #  return "\n#{Phrase.empty}" if result.empty?
-          #
-          #  result.join("\n\n")
-          # end
-
-          # def main_buttons
-          #  buttons_actions = []
-          #  Teachbase::Bot::Interfaces::Section::CHOOSING_BUTTONS.each do |choose_button|
-          #    buttons_actions << router.g(:cs, :sections, id: entity.tb_id, p: [param: choose_button]).link
-          #  end
-          #  InlineCallbackKeyboard.g(buttons_signs: to_i18n(Teachbase::Bot::Interfaces::Section::CHOOSING_BUTTONS),
-          #                           buttons_actions: buttons_actions,
-          #                           back_button: back_button).raw
-          # end
 
           def contents_buttons
             buttons_list = []
